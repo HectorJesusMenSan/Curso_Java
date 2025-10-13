e8f2f9a93b2489ca8b76d4cf23339c7b76cf3bdd
sc03m14 <sc03m14@sc03m14-OptiPlex-SFF-Plus-7010>
2025-10-13 16:09:50 -0700

    fsdfsdfsdfd

---
+++ claseAbstracta/src/figuras/Figura.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package figuras;
+
+/**
+ *
+ * @author sc03m14
+ */    //Se define clase abstracta
+public abstract class Figura {
+
+    private String color;
+    
+}
