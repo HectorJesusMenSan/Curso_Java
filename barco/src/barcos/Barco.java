cee29ac718382f8a360da145903b3e5ba7229d64
sc03m14 <>
2025-10-16 15:39:26 -0700

    nuevo

---
+++ barco/src/barcos/Barco.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package barcos;
+public interface Barco {
+    //Definicion de metodos
+    void alarma();
+    void msgSocorro(String mensaje);
+    
+}
+
