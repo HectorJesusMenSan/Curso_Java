def669239cd0616a392d3a7c4ef1c3eb12258908
sc03m14 <sc03m14@sc03m14-OptiPlex-SFF-Plus-7010>
2025-10-17 15:39:32 -0700

    dasdas

---
+++ src/Cursos/Curso.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Cursos;
+import java.util.ArrayList;
+/**
+ *
+ * @author sc03m14
+ */
+public class Curso {
+
+    //Definir atributos:
+    private String nombre;
+    private String profesor;
+    private ArrayList <Estudiante> listaEstudiante;
+    
+    //Definir constructores
+    //Definir metodos
+    //Definir clase interna
+    class Estudiante{
+        private String nombre;
+        private int edad;
+        //Constructor vacio
+        public Estudiante(String nombre, int edad){
+            this.nombre = nombre;
+            this.edad = edad;
+            
+        }
+        //Implementamos metodos de acceso
+        //Metodos con funcionalidad
+        
+        
+    }
+    
+    
+}
